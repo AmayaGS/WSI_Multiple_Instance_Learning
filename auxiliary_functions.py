f180468132dc5db663986428112897bd7cbd2472
AmayaGS <21223530+AmayaGS@users.noreply.github.com>
2023-01-24 14:03:44 +0000

    Add files via upload

---
+++ auxiliary_functions.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 24 14:03:24 2023
+
+@author: AmayaGS
+"""
+
+class Accuracy_Logger(object):
+    
+    """Accuracy logger"""
+    def __init__(self, n_classes):
+        super(Accuracy_Logger, self).__init__()
+        self.n_classes = n_classes
+        self.initialize()
+
+    def initialize(self):
+        self.data = [{"count": 0, "correct": 0} for i in range(self.n_classes)]
+    
+    def log(self, Y_hat, Y):
+        Y_hat = int(Y_hat)
+        Y = int(Y)
+        self.data[Y]["count"] += 1
+        self.data[Y]["correct"] += (Y_hat == Y)
+    
+    def log_batch(self, Y_hat, Y):
+        Y_hat = np.array(Y_hat).astype(int)
+        Y = np.array(Y).astype(int)
+        for label_class in np.unique(Y):
+            cls_mask = Y == label_class
+            self.data[label_class]["count"] += cls_mask.sum()
+            self.data[label_class]["correct"] += (Y_hat[cls_mask] == Y[cls_mask]).sum()
+    
+    def get_summary(self, c):
+        count = self.data[c]["count"] 
+        correct = self.data[c]["correct"]
+        
+        if count == 0: 
+            acc = None
+        else:
+            acc = float(correct) / count
+        
+        return acc, correct, count
